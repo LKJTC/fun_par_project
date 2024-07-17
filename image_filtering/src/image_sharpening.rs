d3c4104a2f94120d72eabc6f4a90f25f955d4dfc
mobsoicy <keonhee.kim1@gmail.com>
2024-07-18 02:57:17 +0700

    Tidied up image_sharpening.rs

--- image_filtering/src/image_sharpening.rs
+++ image_filtering/src/image_sharpening.rs
@@ -24,7 +24,6 @@ Finally, the calculated pixel values are written to the output_img buffer.
 pub fn sharpen(img: &DynamicImage) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
     let (width, height) = img.dimensions();
     let mut output_img = ImageBuffer::new(width, height);
-
     let offsets = [
         (-1, -1),
         (0, -1),
@@ -36,13 +35,11 @@ pub fn sharpen(img: &DynamicImage) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
         (0, 1),
         (1, 1),
     ];
-
     for y in 1..height - 1 {
         for x in 1..width - 1 {
             let mut r = 0i32;
             let mut g = 0i32;
             let mut b = 0i32;
-
             for &(dx, dy) in &offsets {
                 let pixel = img.get_pixel((x as i32 + dx) as u32, (y as i32 + dy) as u32);
                 let weight = if dx == 0 && dy == 0 { 9 } else { -1 };
@@ -50,18 +47,15 @@ pub fn sharpen(img: &DynamicImage) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
                 g += pixel[1] as i32 * weight;
                 b += pixel[2] as i32 * weight;
             }
-
             let pixel = Rgba([
                 r.clamp(0, 255) as u8,
                 g.clamp(0, 255) as u8,
                 b.clamp(0, 255) as u8,
                 img.get_pixel(x, y)[3],
             ]);
-
             output_img.put_pixel(x, y, pixel);
         }
     }
-
     output_img
 }
 
@@ -74,7 +68,6 @@ Finally, in a loop the calculated pixel values are written to the output_img_buf
 #[allow(dead_code)]
 pub fn par_sharpen(img: &DynamicImage) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
     let (width, height) = img.dimensions();
-
     let offsets = [
         (-1, -1),
         (0, -1),
@@ -86,7 +79,6 @@ pub fn par_sharpen(img: &DynamicImage) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
         (0, 1),
         (1, 1),
     ];
-
     let output_img: Vec<Vec<Rgba<u8>>> = (0..height)
         .into_par_iter()
         .map(|y| {
@@ -95,11 +87,9 @@ pub fn par_sharpen(img: &DynamicImage) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
                     if x == 0 || y == 0 || x == width - 1 || y == height - 1 {
                         return img.get_pixel(x, y);
                     }
-
                     let mut r = 0i32;
                     let mut g = 0i32;
                     let mut b = 0i32;
-
                     for &(dx, dy) in &offsets {
                         let pixel = img.get_pixel((x as i32 + dx) as u32, (y as i32 + dy) as u32);
                         let weight = if dx == 0 && dy == 0 { 9 } else { -1 };
@@ -107,7 +97,6 @@ pub fn par_sharpen(img: &DynamicImage) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
                         g += pixel[1] as i32 * weight;
                         b += pixel[2] as i32 * weight;
                     }
-
                     Rgba([
                         r.clamp(0, 255) as u8,
                         g.clamp(0, 255) as u8,
@@ -118,14 +107,12 @@ pub fn par_sharpen(img: &DynamicImage) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
                 .collect()
         })
         .collect();
-
     let mut output_img_buffer = ImageBuffer::new(width, height);
     for (y, row) in output_img.iter().enumerate() {
         for (x, &pixel) in row.iter().enumerate() {
             output_img_buffer.put_pixel(x as u32, y as u32, pixel);
         }
     }
-
     output_img_buffer
 }
 
